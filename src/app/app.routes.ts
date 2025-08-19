59c0133de9899d0614bbca6f3a91ee332c40a7e5
Matheus Diniz <matheus.gruber123@gmail.com>
2025-08-19 17:29:36 -0300

    testes de aprendizado
    Testes para aprender novas coisas
    tudo que foi usado aqui foi apenas para aprendizado, nada será de fato usado no projeto final.
    
    Retornar pro ultimo commit após encerrado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { Home } from './home/home';
 import { About } from './about/about';
+import { Skills } from './skills/skills';
+import { Projects } from './projects/projects';
+
 
 
 
 export const routes: Routes = [
      {path: '', component: Home},
      {path: 'sobre', component: About},
+     {path: 'habilidades', component: Skills},
+     {path: 'projetos', component: Projects},
 ];
