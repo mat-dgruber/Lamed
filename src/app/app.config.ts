ad9d15740e7fd9c5d908126dbb3faf32ba8e3aef
Matheus Diniz <matheus.gruber123@gmail.com>
2025-08-20 10:54:06 -0300

    Nova Base

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideIcons } from '@ng-icons/core';
-import { lucideMail, lucideMailOpen, lucideCircleDollarSign, lucideYoutube, lucideInstagram, lucideArrowUpFromDot } from '@ng-icons/lucide';
 import { routes } from './app.routes';
+
+import { LucideAngularModule, Menu, Mail, BadgeDollarSign, Youtube, Instagram, ArrowUpFromDot, Send } from 'lucide-angular';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideIcons({ lucideMail, lucideMailOpen, lucideCircleDollarSign, lucideYoutube, lucideInstagram, lucideArrowUpFromDot })
+   
+     importProvidersFrom(LucideAngularModule.pick({ 
+      Menu, 
+      Mail, 
+      BadgeDollarSign, 
+      Youtube, 
+      Instagram, 
+      ArrowUpFromDot,
+      Send, 
+    }))
   ]
 };
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router'; // <-- Adicione esta linha
+import { LucideAngularModule, Menu } from 'lucide-angular'; // <-- Importa o módulo LucideAngularModule
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink], // <-- E adicione aqui
+  imports: [RouterLink, LucideAngularModule], // <-- E adicione aqui
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
