d00571a931fb1d87fa84f36919e5c08dbd4af582
Matheus Diniz <matheus.gruber123@gmail.com>
2025-08-19 21:20:39 -0300

    início

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,17 @@ import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { Home } from './home/home';
 import { About } from './about/about';
-import { Skills } from './skills/skills';
-import { Projects } from './projects/projects';
-import { Contact } from './contact/contact';
-
+import { Videos } from './videos/videos';
+import { Artigos } from './artigos/artigos';
+import { Apoie } from './apoie/apoie';
 
 
 
 export const routes: Routes = [
      {path: '', component: Home},
      {path: 'sobre', component: About},
-     {path: 'habilidades', component: Skills},
-     {path: 'projetos', component: Projects},
-     {path: 'contato', component: Contact},
+     {path: 'videos', component: Videos},
+     {path: 'artigos', component: Artigos},
+     {path: 'apoie', component: Apoie},
+
 ];
