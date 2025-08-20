99ea4052b113c712334beacbf3e2a121dd02a4cb
Matheus Diniz <matheus.gruber123@gmail.com>
2025-08-19 22:59:21 -0300

    Revert "Fix routes, update links, and refactor components"
    This reverts commit 5895e7b4528f5530727cd665baa1553f20ffe7b4.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { Home } from './home/home';
-import { Sobre } from './sobre/sobre';
+import { About } from './about/about';
 import { Videos } from './videos/videos';
 import { Artigos } from './artigos/artigos';
 import { Apoie } from './apoie/apoie';
@@ -11,7 +11,7 @@ import { Apoie } from './apoie/apoie';
 
 export const routes: Routes = [
      {path: '', component: Home},
-     {path: 'sobre', component: Sobre},
+     {path: 'sobre', component: About},
      {path: 'videos', component: Videos},
      {path: 'artigos', component: Artigos},
      {path: 'apoie', component: Apoie},
