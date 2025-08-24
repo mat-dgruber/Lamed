0ef38891f73e740e1a1c68322dcfdb59fcd2a8e9
Matheus Diniz <matheus.gruber123@gmail.com>
2025-08-24 16:46:00 -0300

    Termos e Politica

---
+++ src/app/termos/termos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termos',
+  imports: [],
+  templateUrl: './termos.html',
+  styleUrl: './termos.css'
+})
+export class Termos {
+
+}
