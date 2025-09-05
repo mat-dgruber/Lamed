97432609a64974646bb55bc64b016ddcc8080468
Matheus Diniz <matheus.gruber123@gmail.com>
2025-09-05 19:39:33 -0300

    Siga-nos pagina nova

---
+++ src/app/siga-nos/siga-nos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-siga-nos',
+  imports: [],
+  templateUrl: './siga-nos.html',
+  styleUrl: './siga-nos.css'
+})
+export class SigaNos {
+
+}
