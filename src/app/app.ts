3f783643499c49e5d96787140fe4e95f7a87ec80
Matheus Diniz <matheus.gruber123@gmail.com>
2025-09-14 11:37:37 -0300

    Analytics

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from "./footer/footer";
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs';
+
+// Declara a função gtag para que o TypeScript não reclame, pois ela é carregada globalmente pelo script no index.html
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -11,6 +15,21 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.css',
   standalone: true,
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('angular-lamed');
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Envia um evento de page_view para o Google Analytics
+      gtag('event', 'page_view', {
+        page_title: this.title(), // ou um título mais dinâmico se disponível
+        page_location: event.urlAfterRedirects,
+        page_path: event.urlAfterRedirects
+      });
+    });
+  }
 }
